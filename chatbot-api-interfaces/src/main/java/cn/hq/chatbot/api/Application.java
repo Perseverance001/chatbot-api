991ee7a85d51e37b0450cab83cad0981377d6043
Perseverance001 <1817117243@qq.com>
2024-06-21 23:04:28 +0800

    知识星球与OpenAI API整合

--- chatbot-api-interfaces/src/main/java/cn/hq/chatbot/api/Application.java
+++ chatbot-api-interfaces/src/main/java/cn/hq/chatbot/api/Application.java
@@ -1,10 +1,12 @@
 package cn.hq.chatbot.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 //20240616 hq
 //启动入口
 @SpringBootApplication
+@ComponentScan(basePackages = {"cn.hq.chatbot"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
